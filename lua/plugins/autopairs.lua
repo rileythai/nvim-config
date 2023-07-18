9337af4ec3e6e3509a20aa8c01356c7c8bc062c5
Riley Thai <rileythai@proton.me>
2023-07-18 20:31:04 +1000

    fix: disable autopairs instead of surround

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,6 +1,6 @@
 return {
   -- disable stupid mini autopairs
-  { "echasnovski/mini.surround", enabled = false },
+  { "echasnovski/mini.autopairs", enabled = false },
 
   -- get better autopairs
   { "windwp/nvim-autopairs", event = "InsertEnter", opts = { map_bs = false, map_cr = false } },
