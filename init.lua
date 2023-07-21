a2e86fd41f97e0b33ffd1d8a541b021438a52c9c
Riley Thai <rileythai@proton.me>
2023-07-22 09:48:05 +1000

    fix merge

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 package.loaded["lazyvim.config.options"] = true -- prevent loading of default options
-require("config.autopairs")
 require("config.lazy")
 require("config.autopairs")
 require("config.coqlsp")
