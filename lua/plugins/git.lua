7d607b20af17c602695effc6f723cc37d78a4988
Riley Thai <rileythai@proton.me>
2024-03-08 20:01:09 +1100

    feat: gitsigns diff view + hunk staging
    credit to @yungthai for these specifications from his cfg
    
    squash: fix binds for hunks for simplicity + fix toggle del and diffview binds

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,106 @@
--- temp return blank while i work this out
-if true then
-  return {}
-end
+return {
+  { "sindrets/diffview.nvim" },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "│" },
+          change = { text = "│" },
+          delete = { text = "_" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked = { text = "┆" },
+        },
+        signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+        numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+        watch_gitdir = {
+          follow_files = true,
+        },
+        attach_to_untracked = true,
+        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+          delay = 1000,
+          ignore_whitespace = false,
+        },
+        current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+        sign_priority = 6,
+        update_debounce = 100,
+        status_formatter = nil, -- Use default
+        max_file_length = 40000, -- Disable if file is longer than this (in lines)
+        preview_config = {
+          -- Options passed to nvim_open_win
+          border = "single",
+          style = "minimal",
+          relative = "cursor",
+          row = 0,
+          col = 1,
+        },
+        yadm = {
+          enable = false,
+        },
 
-return { { "tpope/vim.fugitive", keys = { "<leader>gp" } } }
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map("n", "]c", function()
+            if vim.wo.diff then
+              return "]c"
+            end
+            vim.schedule(function()
+              gs.next_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          map("n", "[c", function()
+            if vim.wo.diff then
+              return "[c"
+            end
+            vim.schedule(function()
+              gs.prev_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          -- Actions
+          map("n", "<leader>gh", gs.stage_hunk, { desc = "Stage hunk" })
+          map("n", "<leader>gr", gs.reset_hunk, { desc = "Reset hunk" })
+          map("v", "<leader>gh", function()
+            gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end, { desc = "Stage selected hunk" })
+          map("v", "<leader>gr", function()
+            gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end, { desc = "Reset selected hunk" })
+          map("n", "<leader>gH", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+          map("n", "<leader>gB", function()
+            gs.blame_line({ full = true })
+          end, { desc = "Show full blames" })
+          map("n", "<leader>gb", gs.toggle_current_line_blame, { desc = "Toggle Blame" })
+          map("n", "<leader>gd", function()
+            gs.diffthis(nil, { split = "rightbelow" })
+          end, { desc = "Show diff" })
+          map("n", "<leader>gD", function()
+            gs.diffthis("~", { split = "rightbelow" })
+          end, { desc = "Show diff vs. prev commit" })
+          map("n", "<leader>gx", gs.toggle_deleted, { desc = "Toggle deleted lines" })
+
+          -- Text object
+          map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+        end,
+      })
+    end,
+  },
+}
