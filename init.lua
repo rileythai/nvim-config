8a694a7b6b13ad5103e1c03eb8de2b9be7f3236f
Riley Thai <rileythai@proton.me>
2024-03-11 17:06:26 +1100

    move: coq fixes -> config + init functions
    now does coq stuff there
    still todo:
    - make <CR> do whatever is in the open menu
    - luasnip coq (my own work)

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 package.loaded["lazyvim.config.options"] = true -- prevent loading of default options
 require("config.lazy")
-require("config.autopairs")
-require("config.coqlsp")
--- lua/plugins/coq.lua
+++ lua/plugins/coq.lua
@@ -1,21 +1,93 @@
 return {
+  -- signatures (what function vars do)
+  {
+    "ray-x/lsp_signature.nvim",
+    opts = {},
+    config = function(_, opts)
+      require("lsp_signature").setup(opts)
+    end,
+  },
+  -- the completion engine, coq (sometimes slow)-
   {
     "ms-jpq/coq_nvim",
     event = "InsertEnter",
+    dependencies = { "L3MON4D3/LuaSnip", "" },
+    config = function()
+      local lsp = require("lspconfig")
+      local coq = require("coq")
+
+      -- servers to be loaded into COQ
+      local servers = {
+        "pyright",
+        "texlab",
+        "lua_ls",
+        "matlab_ls",
+        "vale_ls",
+        "grammarly",
+        "ruff_lsp",
+        "arduino_language_server",
+        "cssls",
+        "clangd",
+      }
+      for _, srv in ipairs(servers) do
+        lsp[srv].setup(coq.lsp_ensure_capabilities())
+      end
+    end,
     init = function()
+      local remap = vim.api.nvim_set_keymap
+      local npairs = require("nvim-autopairs")
+
+      npairs.setup({ map_bs = false, map_cr = false })
+
+      -- all settings (any other will override this)
       vim.g.coq_settings = {
         auto_start = "shut-up",
+        keymap = {
+          recommended = false,
+          jump_to_mark = "",
+        },
+        clients = { snippets = { enabled = false } },
       }
+
+      -- these mappings are coq recommended mappings unrelated to nvim-autopairs
+      remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
+      remap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
+
+      -- skip it, if you use another global object
+      _G.MUtils = {}
+
+      MUtils.CR = function()
+        if vim.fn.pumvisible() ~= 0 then
+          if vim.fn.complete_info({ "selected" }).selected ~= -1 then
+            return npairs.esc("<c-y>")
+          else
+            return npairs.esc("<c-e>") .. npairs.autopairs_cr()
+          end
+        else
+          return npairs.autopairs_cr()
+        end
+      end
+      remap("i", "<cr>", "v:lua.MUtils.CR()", { expr = true, noremap = true })
+
+      MUtils.BS = function()
+        if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ "mode" }).mode == "eval" then
+          return npairs.esc("<c-e>") .. npairs.autopairs_bs()
+        else
+          return npairs.autopairs_bs()
+        end
+      end
+      remap("i", "<bs>", "v:lua.MUtils.BS()", { expr = true, noremap = true })
     end,
   },
-  { "ms-jpq/coq.artifacts" },
   {
     "ms-jpq/coq.thirdparty",
-    opts = {
-      { src = "nvimlua", short_name = "nLUA" },
-      { src = "vimtex", short_name = "vTEX" },
-      { src = "copilot", short_name = "COP", accept_key = "<c-f>" },
-    },
+    config = function()
+      require("coq_3p")({
+        { src = "nvimlua", short_name = "nLUA" },
+        { src = "vimtex", short_name = "vTEX" },
+        { src = "copilot", short_name = "COP", accept_key = "<c-f>" },
+      })
+    end,
   },
   {
     "kawre/neotab.nvim",
@@ -48,11 +120,12 @@ return {
       },
     },
   },
+  { "mendes-davi/coq_luasnip" },
   {
     "L3MON4D3/LuaSnip",
     build = "make install_jsregexp",
     event = "InsertEnter",
-    dependencies = { "neotab.nvim" },
+    dependencies = { "neotab.nvim", "mendes-davi/coq_luasnip" },
     keys = {
       {
         "<Tab>",
