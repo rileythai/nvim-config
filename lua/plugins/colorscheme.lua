a57cdb1b538aa4ac61a2c51b538810123ad7cc07
Riley Thai <rileythai@proton.me>
2024-07-12 09:03:10 +1000

    update: binds n that n this idk

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,5 +2,7 @@
 return {
   { "rebelot/kanagawa.nvim", config = { theme = "dragon" } },
   { "junegunn/seoul256.vim" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "rose-pine/neovim", name = "rose-pine" },
   { "shaunsingh/nord.nvim" },
 }
