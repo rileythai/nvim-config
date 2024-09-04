77343d4989bd57ac689e3687a4122c94f0109f04
Riley Thai <rileythai@proton.me>
2024-09-04 12:09:48 +1000

    fix: basedpyright warnings
    - will only shut up about the strict imports now

--- lua/plugins/basedpyright.lua
+++ lua/plugins/basedpyright.lua
@@ -1,18 +1,18 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      basedpyright = {
-        settings = {
-          disableOrganizeImports = true,
-          basedpyright = {
-            analysis = {
-              ignore = { "*" },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              analysis = {
+                typeCheckingMode = "standard",
+              },
             },
           },
         },
       },
-      ruff_lsp = {},
     },
   },
 }
